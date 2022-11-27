5e20a580179f9051da2240a7fc1cad495fdc1e07
ksy133900 <tkdgns13578@gmail.com>
2022-11-27 14:37:18 +0900

    회원가입 및 로그인 기능 간단 구현

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    # username = models.CharField(max_length=16, unique=True)
+    nickname = models.CharField(max_length=20, unique=True, null=True)
+    followings = models.ManyToManyField(
+        "self", symmetrical=False, related_name="followers"
+    )
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import get_user_model
+from .forms import *
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login as auth_login
+
 
 # Create your views here.
+def signup(request):
+
+    if request.user.is_authenticated:
+        return redirect("review:index")
+    # 로그인 상태면 회원가입 ㄴㄴ
+    if request.method == "POST":
+        signup_form = CustomUserCreationForm(request.POST)
+        if signup_form.is_valid():
+            user = signup_form.save()
+
+            return redirect("review:index")
+    else:
+        signup_form = CustomUserCreationForm()
+
+    context = {
+        "signup_form": signup_form,
+    }
+
+    return render(request, "accounts/signup.html", context)
+
+
+def login(request):
+    # 로그인 상태면 로그인 ㄴㄴ
+    if request.user.is_authenticated:
+        return redirect("review:index")
+
+    if request.method == "POST":
+        login_form = AuthenticationForm(request, data=request.POST)
+        if login_form.is_valid():
+            auth_login(request, login_form.get_user())
+            return redirect(request.GET.get("next") or "review:index")
+    else:
+        login_form = AuthenticationForm()
+
+    context = {
+        "login_form": login_form,
+    }
+
+    return render(request, "accounts/login.html", context)
