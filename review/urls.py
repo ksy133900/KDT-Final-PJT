443e76f214188c30e93b6c36a1999de829c676bc
leesc8408 <leesc8408@gmail.com>
2022-11-28 18:21:15 +0900

    장르목록 페이지 생성

--- review/urls.py
+++ review/urls.py
@@ -6,6 +6,7 @@ app_name = "articles"
 urlpatterns = [
     path("", views.pro_index, name="pro_index"),
     path("index/", views.index, name="index"),
+    path("genre/", views.genre, name="genre"),
     # path("create/", views.create, name="create"),
     # path("detail/", views.detail, name="detail"),
 ]
--- review/views.py
+++ review/views.py
@@ -10,7 +10,13 @@ def pro_index(request):
 def index(request):
     # review = Review.objects.order_by("-pk")
     return render(request, "review/index.html")
-
+    
+def genre(request):
+    test = [0,1,2,3,4,5,6,7,8,9]
+    context = {
+        "test": test
+    }
+    return render(request, "review/genre.html", context)
 
 # def create(request):
 
