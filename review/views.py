3d3cc689665489ce1c6d601d05f7b51de5284f66
ksy133900 <tkdgns13578@gmail.com>
2022-11-28 14:14:26 +0900

    글 생성 추가

--- review/views.py
+++ review/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import *
+from .forms import *
 
 # Create your views here.
 
@@ -9,9 +10,46 @@ def index(request):
     return render(request, "review/index.html")
 
 
-# def create(request):
+def create(request):
+    if request.method == "POST":
+        review_form = ReviewForm(request.POST, request.FILES)
+        photo_form = PhotoForm(request.POST, request.FILES)
+        images = request.FILES.getlist("image")
+        # tags = request.POST.get("tags", "").split(",")
 
-#     return render(request, "review/create.html")
+        # if request.POST.get("tags", "") != "":
+        #     tags = request.POST.get("tags", "").split(",")
+        # else:
+        #     tags = None
+
+        if review_form.is_valid() and photo_form.is_valid():
+            review = review_form.save(commit=False)
+            review.user = request.user
+
+            if len(images):
+                for image in images:
+                    image_instance = Photo(review=review, image=image)
+                    review.save()
+                    image_instance.save()
+
+            review.save()
+            # if tags:
+            #     for tag in tags:
+            #         tag = tag.strip()
+            #         article.tags.add(tag)
+            #         article.save()
+
+            return redirect("review:index")
+    else:
+        review_form = ReviewForm()
+        photo_form = PhotoForm()
+    context = {
+        "review_form": review_form,
+        "photo_form": photo_form,
+    }
+    return render(request, "review/create.html", context)
+
+
 # def detail(request):
 
 #     return render(request, "review/detail.html")
