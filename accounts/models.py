353a84e64b075721bd1df4ab6ab8aa4d9327215a
ksy133900 <tkdgns13578@gmail.com>
2022-11-28 13:59:00 +0900

    모델,폼(회원정보수정은 아직x) 추가

--- accounts/models.py
+++ accounts/models.py
@@ -1,10 +1,29 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from imagekit.models import ProcessedImageField
+from imagekit.processors import ResizeToFill
+from django.conf import settings
 
 # Create your models here.
 class User(AbstractUser):
     # username = models.CharField(max_length=16, unique=True)
     nickname = models.CharField(max_length=20, unique=True, null=True)
+    age = models.IntegerField(null=True)
     followings = models.ManyToManyField(
         "self", symmetrical=False, related_name="followers"
     )
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    intro = models.TextField(blank=True)  # 소개글
+    image = ProcessedImageField(
+        blank=True,
+        upload_to="profile/",
+        processors=[ResizeToFill(300, 300)],
+        format="JPEG",
+        options={"quality": 90},
+    )
+
+    def __str__(self):
+        return self.user.email
