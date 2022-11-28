9576e941f157b60bcff82f3cb4afc26b58172be0
ksy133900 <tkdgns13578@gmail.com>
2022-11-28 14:15:56 +0900

    review 모델 작성

--- review/models.py
+++ review/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+from django.conf import settings
+from taggit.managers import TaggableManager
 
 # Create your models here.
+class Review(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=20)
+    content = models.TextField()
+    genre_choice = [
+        ("추리", "추리"),
+        ("스릴러", "스릴러"),
+        ("공포", "공포"),
+        ("판타지", "판타지"),
+        ("로맨스", "로맨스"),
+    ]
+    genre = models.CharField(max_length=20, choices=genre_choice)
+    age = models.IntegerField(null=True)
+    # 조회수
+    view_count = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    location = models.CharField(max_length=80, blank=True)
+    tags = TaggableManager(blank=True)
+    modify_dt = models.DateTimeField("MODIFY DATE", auto_now=True)
+    like_users = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, related_name="like_reviews"
+    )
+    # bookmark_users = models.ManyToManyField(
+    #     settings.AUTH_USER_MODEL, related_name="bookmark_reviews"
+    # )
+
+    def __str__(self):
+        return self.title
+
+
+class Photo(models.Model):
+    review = models.ForeignKey(Review, on_delete=models.CASCADE, null=True)
+    image = models.ImageField(upload_to="images/", blank=True, null=True)
+
+
+class Comment(models.Model):
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    review = models.ForeignKey(Review, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    like_users = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, related_name="like_comments"
+    )
+    parent_comment = models.ForeignKey("self", on_delete=models.CASCADE, null=True)
