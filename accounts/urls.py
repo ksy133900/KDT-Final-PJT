1e7b33dc0bb8235e76d65afd80430d24cafcc114
leesc8408 <leesc8408@gmail.com>
2022-11-29 16:32:25 +0900

    공개프로필 파일명 변경

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,6 +9,5 @@ urlpatterns = [
     path("login/", views.login, name="login"),
     path("logout/", views.logout, name="logout"),
     # path("<int:pk>/", views.detail, name="detail"),
-    path("profile/", views.profile, name="profile"),# 이후에 <int:pk>넣어 주세요~
-    path("update/", views.update, name="update"),# 이후에 <int:pk>넣어 주세요~
+    path("open_profile/", views.open_profile, name="open_profile"),# 이후에 <int:pk>넣어 주세요~
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -51,8 +51,8 @@ def logout(request):
     auth_logout(request)
     return redirect("review:index")
 
-def profile(request):
-    return render(request,"accounts/profile.html")
+def open_profile(request):
+    return render(request,"accounts/open_profile.html")
 
 def update(request):
     return render(request,"accounts/update.html")    
