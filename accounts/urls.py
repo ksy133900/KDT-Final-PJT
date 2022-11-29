a806ba9dd81df83c4cce8c0d9a79c23889152c40
leesc8408 <leesc8408@gmail.com>
2022-11-29 13:34:54 +0900

    review/detail,accounts/update_html 생성

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path("login/", views.login, name="login"),
     path("logout/", views.logout, name="logout"),
     # path("<int:pk>/", views.detail, name="detail"),
-    path("profile/", views.profile, name="profile"),# 이후에 <int:pk>넣어야함
+    path("profile/", views.profile, name="profile"),# 이후에 <int:pk>넣어 주세요~
+    path("update/", views.update, name="update"),# 이후에 <int:pk>넣어 주세요~
 ]
