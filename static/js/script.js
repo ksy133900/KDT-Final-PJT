f9ca1e2351ab958f6d9718ce35d417dffb213102
wsm0409 <wsm0409@naver.com>
2022-12-01 00:18:56 +0900

    버튼효과에대한의견부탁,검색창구현중

--- static/js/script.js
+++ static/js/script.js
@@ -3,31 +3,32 @@ const books = [
     id: 1,
     name: '세계문학',
     genre: '공포',
-    url: './static/images/세계문학.jpg'
+    url: '/../../../static/images/1.jpg'
   },
   {
     id: 2,
     name: '우리문학',
     genre: '추리',
-    url: 'static/images/우리문학.jpg'
+    url: '/../../../static/images/2.jpg'
   },
   {
     id: 3,
     name: '죽음문학',
     genre: '로맨스',
-    url: '../죽음문학.jpg'
+    url: '/../../../static/images/3.jpg'
   },
   {
     id: 4,
     name: '환상문학',
     genre: '판타지',
-    url: 'static/images/환상문학.jpg'
+    url: '/../../static/images/4.jpg'
   },
-]
+];
+
 
 const list = document.getElementById('list');
 
-function showList(val) {
+function showList(val = '') {
   list.innerHTML = '';
   const res = books.forEach(book => {
     if (book.name.includes(val)) {
@@ -44,12 +45,43 @@ function showList(val) {
 
 showList('');
 
+
+const searchFilter = (data, keyword) => {
+
+  return data.filter((item) => (item.name.indexOf(keyword) != -1));
+};
+
+const conatiner = document.querySelector(".container");
+conatiner.style.display = 'none';
+conatiner.innerHTML = books.map((book) => `<li>${book.name}</li>`).join("");
+
+const input = document.querySelector(".search-txt");
+input.addEventListener("keyup", (e) => {
+  const keyword = e.target.value;
+  const filterdbooks = searchFilter(books, keyword);
+  conatiner.innerHTML = filterdbooks
+    .map((book) => `<li>${book.name}</li>`)
+    .join("");
+});
+
+
+
 const searchInput = document.getElementById('search');
 const searchBtn = document.getElementById('searchBtn');
 
 searchBtn.addEventListener('click', (e) => {
   e.preventDefault();
+  window.location.href = "/matching/" + "?" + "q=" + searchInput.value;
   const val = searchInput.value;
   console.log(val);
   showList(val);
 })
+const form = document.getElementsByClassName('container')[0];
+
+searchInput.addEventListener('focusin', (event) => {
+  form.style.display = 'initial';
+});
+
+searchInput.addEventListener('focusout', (event) => {
+  form.style.display = 'none';
+});
