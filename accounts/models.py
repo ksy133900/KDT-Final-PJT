9581d001a57110727c63fa594d25cc3b6619e929
ksy133900 <tkdgns13578@gmail.com>
2022-12-01 16:42:44 +0900

    accounts 앱에 주소랑 선호나이 모델 추가

--- accounts/models.py
+++ accounts/models.py
@@ -12,6 +12,7 @@ class User(AbstractUser):
     followings = models.ManyToManyField(
         "self", symmetrical=False, related_name="followers"
     )
+    address = models.CharField(max_length=50, null=True)
 
 
 class Profile(models.Model):
@@ -54,6 +55,17 @@ class Profile(models.Model):
         ("저녁", "저녁 : 18:00 ~ 23:59 사이"),
     ]
     time = models.CharField(max_length=20, choices=times, default="시간")  # 선호 시간
+    age_select = [
+        ("선택해주세요", "선택해주세요"),
+        ("20대", "20대"),
+        ("30대", "30대"),
+        ("40대", "40대"),
+        ("50대", "50대"),
+    ]
+    ages = models.CharField(
+        max_length=20, default="선택해주세요", choices=age_select
+    )  # 선호 장르
+    location = models.CharField(max_length=20, null=True)
 
     def __str__(self):
         return self.user.email
