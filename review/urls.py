91f0092d2ca5a82793feeff856f95af766d4cade
GitHub etdong0820 <etd93@naver.com>
2022-12-02 19:35:24 +0900

    본인_독토요청 안보기(수정중), 글 수정, 글 삭제(완료)_글 수정 이미지없음(오류사항)

--- review/urls.py
+++ review/urls.py
@@ -10,4 +10,8 @@ urlpatterns = [
     path("create/", views.create, name="create"),
     path("detail/", views.detail, name="detail"),
     path("matching/", views.matching, name="matching"),
+    #글 수정
+    path("update/<int:pk>", views.update, name="update"),
+    # 글 삭제
+    path("delete/<int:pk>", views.delete, name="delete"),
 ]
