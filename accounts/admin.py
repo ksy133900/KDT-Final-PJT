d02564531c395b756ee2b9f8035786764c16968b
leesc8408 <leesc8408@gmail.com>
2022-12-02 15:10:48 +0900

    디테일카드 임시저장

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import User, Profile
 
 # Register your models here.
+
+
+# admin.site.register(User)
+# admin.site.register(Profile)
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['id', 'username']
+    list_display_links = ['id', 'username']
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['id', 'nickname']
+    list_display_links = ['id', 'nickname']
