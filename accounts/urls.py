2cd14b070ec30a3444f950324b625e96ffbaf133
milipara <pjun1106@gmail.com>
2022-12-06 18:07:46 +0900

    검색창 미완성1

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-
+from . views import search
 
 app_name = "accounts"
 
--- accounts/views.py
+++ accounts/views.py
@@ -9,6 +9,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.http import JsonResponse
 from review.models import Review
+from django.db.models import Q
 
 # Create your views here.
 def signup(request):
@@ -114,6 +115,41 @@ def delete(request):
 
     return redirect("review:index")
 
+# 검색 기능
+def get_queryset(self):
+    search_keyword = self.request.GET.get('q', '')
+    search_type = self.request.GET.get('type', '')
+    notice_list = Notice.objects.order_by('-id') 
+    
+    if search_keyword :
+        if len(search_keyword) > 0 :
+            if search_type == 'all':
+                search_notice_list = notice_list.filter(Q (title__icontains=search_keyword) | Q (content__icontains=search_keyword) | Q (writer__user_id__icontains=search_keyword))
+            elif search_type == 'title_content':
+                search_notice_list = notice_list.filter(Q (title__icontains=search_keyword) | Q (content__icontains=search_keyword))
+            elif search_type == 'title':
+                search_notice_list = notice_list.filter(title__icontains=search_keyword)    
+            elif search_type == 'content':
+                search_notice_list = notice_list.filter(content__icontains=search_keyword)    
+            elif search_type == 'writer':
+                search_notice_list = notice_list.filter(writer__user_id__icontains=search_keyword)
+
+            return search_notice_list
+        else:
+            messages.error(self.request, '검색어는 1글자 이상 입력해주세요.')
+    return notice_list
+
+# notice/views.py
+
+def get_context_data(self, **kwargs):
+    search_keyword = self.request.GET.get('q', '')
+    search_type = self.request.GET.get('type', '')
+
+    if len(search_keyword) > 1 :
+        context['q'] = search_keyword
+    context['type'] = search_type
+
+    return context
 
 @require_POST
 @login_required
