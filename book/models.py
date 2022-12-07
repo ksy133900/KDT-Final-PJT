b4683a50ef3635e8a3d6032805f42ce44eb6554a
milipara <pjun1106@gmail.com>
2022-12-07 18:43:58 +0900

    도서 모델만들기

--- book/models.py
+++ book/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+User = get_user_model()
+
+class book_genre(models.Model):
+    genre=models.CharField(max_length=10, unique=True)
+    
+    def __str__(self):
+        return self.genre
+    def get_deferred_fields(self):
+        return reverse('book/book_list.html',args=[self.genre])
+    
+class Book(models.Model):
+    #제목
+    title=models.CharField(max_length=50)
+
+    #장르
+    genre=models.ForeignKey(book_genre,max_length=10, blank=True,)
+
+    #가격
+    price=models.PositiveIntegerField()
+
+    # 줄거리
+    summary=models.TextField(max_length=500)
+
+    # 매칭수
+    matching_count = models.PositiveIntegerField() 
 
 # Create your models here.
