d9d42c80f6debe4cffdc2300155ce063e15e34ec
leesc8408 <leesc8408@gmail.com>
2022-12-08 17:52:59 +0900

    도서정보 입력 임시적용/리뷰작성(create오류??)

--- book/admin.py
+++ book/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Book,book_genre
 
 # Register your models here.
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['title', 'genre']
+    list_display_links = ['title', 'genre']
+
+@admin.register(book_genre)
+class book_genreAdmin(admin.ModelAdmin):
+    list_display = ['genre']
+    list_display_links = ['genre']
