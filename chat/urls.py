bf320df3551c5fd22c0e421d0a36db8f17a6dfcd
GitHub etdong0820 <etd93@naver.com>
2022-12-13 16:11:44 +0900

    chat 불필요한 부분 삭제

--- chat/urls.py
+++ chat/urls.py
@@ -8,13 +8,5 @@ app_name='chat'
 urlpatterns = [
     path("", index, name="index"),
     path("<int:pk>/", views.room, name="room"),
-    # re_path(r'^(?P<room_name>[^/]+)/$', room, name='room'),
-    # path("<int:room_name>/", views.room, name="room"),
-    # path("<int:pk>/find_room/", views.find_room, name="find_room"),
-    # path("<int:pk>/init_room/", views.init_room, name="init_room"),
-    # path("message/", views.message, name="message"),
-    # path("empty/", views.message, name="message"),
-    # path("", views.chatroom, name="chatroom")
-
 
 ]
