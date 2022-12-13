d176e110cedc8ad3476faee9248e53202532e755
ksy133900 <tkdgns13578@gmail.com>
2022-12-13 13:44:47 +0900

    book의 views.py create 수정

--- book/views.py
+++ book/views.py
@@ -7,22 +7,22 @@ def create(request):
     if request.method == "POST":
         book_form = bookForm(request.POST)
         image_form = ImageForm(request.POST, request.FILES)
-        # images = request.FILES.getlist("image")
+        images = request.FILES.getlist("image")
 
         if book_form.is_valid() and image_form.is_valid():
             book = book_form.save(commit=False)
             book.user = request.user
+
+            # images = image_form.save(commit=False)
+            # images.book = book
+            # images.save()
+
+            if len(images):
+                for img in images:
+                    img_instance = Image(book=book, image=img)
+                    book.save()
+                    img_instance.save()
             book.save()
-            images = image_form.save(commit=False)
-            images.book = book
-            images.save()
-            
-            # if len(images):
-            #     for img in images:
-            #         img_instance = Image(book=book, image=img)
-
-            #         img_instance.save()
-
             return redirect("review:index")
     else:
         book_form = bookForm()
@@ -33,7 +33,8 @@ def create(request):
     }
     return render(request, "book/create.html", context)
 
-def delete(request,book_pk):
-    book = Book.objects.get(pk = book_pk)
+
+def delete(request, book_pk):
+    book = Book.objects.get(pk=book_pk)
     book.delete()
     return redirect("review:index")
