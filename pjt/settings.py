cc1baea8016c96c68a2b0342e1f90155148a1657
GitHub etdong0820 <etd93@naver.com>
2022-12-14 02:06:17 +0900

    websocket 서버 연결 문제

--- pjt/settings.py
+++ pjt/settings.py
@@ -68,7 +68,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
          "CONFIG": {
-             "hosts": [("127.0.0.1", 6379)],
+             "hosts": [("Kdtdjangodocto-env.eba-3mhhckaa.ap-northeast-2.elasticbeanstalk.com", 5432)],
          },
     },
 }
