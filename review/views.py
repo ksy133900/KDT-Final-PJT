0497cd867b8b890e0153a8bde21d9cd5226d1d85
leesc8408 <leesc8408@gmail.com>
2022-12-13 16:32:28 +0900

    삭제하러

--- review/views.py
+++ review/views.py
@@ -185,14 +185,14 @@ def delete(request, pk, book_pk):
 def detail(request, book_pk):
     reviews = Review.objects.filter(book_id=book_pk).order_by("-pk")
     book = Book.objects.get(pk=book_pk)
-    book_image = Image.objects.get(book_id=book_pk)
+    # book_image = Image.objects.get(book_id=book_pk)
     genre_list = ["공포/추리" , "판타지", "로맨스/가족", "역사/철학", "정치/경제"]
     genre = genre_list[int(book.genre)-1]
 
     context = {
         "reviews": reviews,
         "book": book,
-        "book_image": book_image,
+        # "book_image": book_image,
         "genre": genre,    
     }
     return render(request, "review/detail.html", context)
