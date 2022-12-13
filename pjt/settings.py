c90f784c3b4f2d5432c7b1f1218408840e635a11
ksy133900 <tkdgns13578@gmail.com>
2022-12-13 14:18:52 +0900

    settings 수정

--- pjt/settings.py
+++ pjt/settings.py
@@ -14,13 +14,11 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
-
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+load_dotenv()
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -103,19 +101,6 @@ TEMPLATES = [
     },
 ]
 
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
-
-
-# Password validation
-# https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -133,9 +118,6 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-# Internationalization
-# https://docs.djangoproject.com/en/3.2/topics/i18n/
-
 LANGUAGE_CODE = "ko-kr"
 
 TIME_ZONE = "Asia/Seoul"
@@ -144,11 +126,8 @@ USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = False
+USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
 STATIC_ROOT = "staticfiles"
@@ -157,16 +136,12 @@ STATICFILES_DIRS = [
 ]
 AUTH_USER_MODEL = "accounts.User"
 
-# settings.py 작성
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEBUG = os.getenv("DEBUG") == "True"
 
 if DEBUG:  # 개발(로컬) 환경
     MEDIA_URL = "/media/"
-    MEDIA_ROOT = BASE_DIR / "images"
+    MEDIA_ROOT = BASE_DIR / "media"
 
     DATABASES = {
         "default": {
@@ -189,21 +164,10 @@ else:  # 배포(원격, 클라우드) 환경
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.postgresql",
-            "NAME": os.getenv("DATABASE_NAME"),  # .env 파일에 value 작성
+            "NAME": os.getenv("DATABASE_NAME"),
             "USER": "postgres",
-            "PASSWORD": os.getenv("DATABASE_PASSWORD"),  # .env 파일에 value 작성
-            "HOST": os.getenv("DATABASE_HOST"),  # .env 파일에 value 작성
+            "PASSWORD": os.getenv("DATABASE_PASSWORD"),
+            "HOST": os.getenv("DATABASE_HOST"),
             "PORT": "5432",
         }
     }
-
-    # DATABASES = {
-    #     "default": {
-    #         "ENGINE": "django.db.backends.postgresql",
-    #         "NAME": os.getenv("DATABASE_NAME"),
-    #         "USER": "postgres",
-    #         "PASSWORD": os.getenv("DATABASE_PASSWORD"),
-    #         "HOST": os.getenv("DATABASE_HOST"),
-    #         "PORT": "5432",
-    #     }
-    # }
