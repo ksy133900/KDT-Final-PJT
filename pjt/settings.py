b6329eb8e9d54895495f34dd82ca0c9a3ed7b9e7
GitHub etdong0820 <etd93@naver.com>
2022-12-14 04:24:41 +0900

    setting 로컬에서 chat가능하게 수정

--- pjt/settings.py
+++ pjt/settings.py
@@ -67,9 +67,9 @@ ASGI_APPLICATION = "pjt.asgi.application"
 CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels.layers.InMemoryChannelLayer",
-         "CONFIG": {
-             "hosts": [("127.0.0.1", 6379)],
-         },
+        #  "CONFIG": {
+        #      "hosts": [("127.0.0.1", 6379)],
+        #  },
     },
 }
 
