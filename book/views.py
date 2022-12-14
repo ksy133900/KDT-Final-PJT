3c8571c09f2d51b8580725a6d002671be9b4b4ed
ksy133900 <tkdgns13578@gmail.com>
2022-12-14 14:26:55 +0900

    책 정보 수정 기능 추가

--- book/views.py
+++ book/views.py
@@ -34,10 +34,46 @@ def create(request):
     return render(request, "book/create.html", context)
 
 
+def update(request, book_pk):
+    book = Book.objects.get(pk=book_pk)
+    imagess = Image.objects.filter(book_id=book.pk)
+    if request.method == "POST":
+        book_form = bookForm(request.POST, instance=book)
+        image_form = ImageForm(request.POST, request.FILES)
+        images = request.FILES.getlist("image")
+
+        for img in imagess:
+            if img.image:
+                img.delete()
+
+        if book_form.is_valid() and image_form.is_valid():
+            book = book_form.save(commit=False)
+
+            if len(images):
+                for image in images:
+                    image_instance = Image(book=book, image=image)
+                    book.save()
+                    image_instance.save()
+            book.save()
+            return redirect("review:index")
+    else:
+        book_form = bookForm(instance=book)
+        if imagess:
+            image_form = ImageForm(instance=imagess[0])
+        else:
+            image_form = ImageForm()
+    context = {
+        "book_form": book_form,
+        "image_form": image_form,
+    }
+    return render(request, "book/create.html", context)
+
+
 def delete(request, book_pk):
     book = Book.objects.get(pk=book_pk)
     book.delete()
     return redirect("review:index")
+
 
 def book_list(request, pk):
     if pk == 1:
@@ -50,11 +86,11 @@ def book_list(request, pk):
         genre = "역사/철학"
     elif pk == 5:
         genre = "정치/경제"
-    book_genre = Book.objects.filter(genre__contains = pk)
+    book_genre = Book.objects.filter(genre__contains=pk)
 
     context = {
         "book_genre": book_genre,
         "genre": genre,
     }
-    
+
     return render(request, "book/book_list.html", context)
